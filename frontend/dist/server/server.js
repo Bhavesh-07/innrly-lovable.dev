2778de6d5709608a591d707f4b054326186df09b
sanjayrathod472 <sanjay.rathod@confidosoft.in>
2026-09-21 14:05:27 +0530

    build: bundle prebuilt production dist for server deployment

--- frontend/dist/server/server.js
+++ frontend/dist/server/server.js
@@ -57,7 +57,7 @@ function renderErrorPage() {
 //#region src/server.ts
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./assets/server-DGdXqI98.js").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./assets/server-B6DD3GN6.js").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 function brandedErrorResponse() {
